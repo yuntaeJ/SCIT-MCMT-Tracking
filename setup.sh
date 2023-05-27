7060a3c5e1d86515c4a0ffbf52431bf19de311ae
Yuntae Jeon <67727745+yuntaeJ@users.noreply.github.com>
2023-05-27 13:31:52 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
+# Step1. Install ByteTrack.
+conda env create --file environment.yaml
+pip3 install -r requirements.txt
+python3 setup.py develop
+
+# Step2. Install pycocotools.
+pip3 install cython; pip3 install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
+
+# Step3. Others
+pip3 install cython_bbox
+pip install mmcv-full==1.7.1
 pip install -v -e .

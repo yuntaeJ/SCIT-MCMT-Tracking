27cf333fc1a4719222b7ad02b6a2c7a468276334
Yuntae Jeon <67727745+yuntaeJ@users.noreply.github.com>
2023-11-18 01:47:29 +0900

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/run_scmt-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-
-# Single-camera Tracking on multicam
-
-python3 tools/multicam_track.py video \
-./test-site022 \
-../experiments/yolox/yolox_x.py \
-../experiments/yolox/epoch_250.pth \
---tp_weight ../tp/tp_best.pth \
---save_result ./test-site022 --save_vid True --track_buffer 150 
